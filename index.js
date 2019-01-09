7681994ea32246bd0cca7e58ac1ee776c656d4e0
EC2 Default User <ec2-user@ip-172-31-10-30.us-east-2.compute.internal>
2019-01-09 13:57:22 +0000

    My first node app

---
+++ index.js
@@ -0,0 +1,9 @@
+var _ = require('underscore');
+
+//Hierarchy of finding the module.
+// Core Module
+// FIle or folder
+// node_modules
+ 
+ var result = _.contains([1,2,3],3)
+ console.log(result);
